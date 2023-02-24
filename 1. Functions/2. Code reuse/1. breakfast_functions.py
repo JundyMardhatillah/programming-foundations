238ba8175da52a65d2c73fb9c30d2d5bade13433
Jundy Mardhatillah <jundeveloper92@gmail.com>
2023-02-24 22:01:44 +0700

    create a function called make_pancake

--- 1. Functions/2. Code reuse/1. breakfast_functions.py
+++ 1. Functions/2. Code reuse/1. breakfast_functions.py
@@ -8,3 +8,10 @@ def make_omelette():
     print('Cooking the other side')
     omelette = 'a tasty omelette'
     return omelette
+
+def make_pancake():
+    print('Mixing the ingredients')
+    print('Pouring the mixture into a frying pan')
+    print('Cooking the first side')
+    print('Flipping it!')
+    print('Cooking the other side')
