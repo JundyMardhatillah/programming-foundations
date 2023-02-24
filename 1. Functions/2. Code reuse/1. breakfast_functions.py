a3cfe061cf2b0a3b5949a28d690727d081b3e6d5
Jundy Mardhatillah <jundeveloper92@gmail.com>
2023-02-24 21:59:05 +0700

    create a function called make_omelette

--- 1. Functions/2. Code reuse/1. breakfast_functions.py
+++ 1. Functions/2. Code reuse/1. breakfast_functions.py
@@ -1,1 +1,8 @@
 """ ===| A Functional Breakfast |=== """
+
+def make_omelette():
+    print('Mixing the ingredients')
+    print('Pouring the mixture into a frying pan')
+    print('Cooking the first side')
+    print('Flipping it!')
+    print('Cooking the other side')
