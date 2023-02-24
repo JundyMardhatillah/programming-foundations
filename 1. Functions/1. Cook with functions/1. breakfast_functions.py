68976df69105e439fefa533aafbc0aa010b0b5d4
Jundy Mardhatillah <jundeveloper92@gmail.com>
2023-02-24 21:36:57 +0700

    add a series of print statement for a functional breakfast

---
+++ 1. Functions/1. Cook with functions/1. breakfast_functions.py
@@ -0,0 +1,7 @@
+""" ===| A Functional Breakfast |=== """
+
+print('Mixing the ingredients')
+print('Pouring the mixture into a frying pan')
+print('Cooking the first side')
+print('Flipping it!')
+print('Cooking the other side')
