328d2773a84df333b00afcd97e95baafc2eb74a3
Jundy Mardhatillah <jundeveloper92@gmail.com>
2023-02-24 22:11:10 +0700

    inserting a new print statement for greasing the frying pan

--- 1. Functions/2. Code reuse/1. breakfast_functions.py
+++ 1. Functions/2. Code reuse/1. breakfast_functions.py
@@ -2,6 +2,7 @@
 
 def mix_and_cook():
     print('Mixing the ingredients')
+    print('Greasing the frying pan')
     print('Pouring the mixture into a frying pan')
     print('Cooking the first side')
     print('Flipping it!')
