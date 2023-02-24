00d2642bcac400c8e53349cbbfb3e25d8f144e24
Jundy Mardhatillah <jundeveloper92@gmail.com>
2023-02-24 22:04:33 +0700

    create the mix and cook function

--- 1. Functions/2. Code reuse/1. breakfast_functions.py
+++ 1. Functions/2. Code reuse/1. breakfast_functions.py
@@ -1,5 +1,12 @@
 """ ===| A Functional Breakfast |=== """
 
+def mix_and_cook():
+    print('Mixing the ingredients')
+    print('Pouring the mixture into a frying pan')
+    print('Cooking the first side')
+    print('Flipping it!')
+    print('Cooking the other side')
+    
 def make_omelette():
     print('Mixing the ingredients')
     print('Pouring the mixture into a frying pan')
