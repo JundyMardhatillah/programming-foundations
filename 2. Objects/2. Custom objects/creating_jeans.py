fbaf1e004cac1bc06a177c1c34ce303d53b94cb3
Jundy Mardhatillah <jundeveloper92@gmail.com>
2023-02-25 23:24:18 +0700

    add init function with self parameter and three properties

--- 2. Objects/2. Custom objects/creating_jeans.py
+++ 2. Objects/2. Custom objects/creating_jeans.py
@@ -1,1 +1,9 @@
 """ ===| The Blueprints for Jeans |=== """
+
+class jeans:
+
+    def __init__(self, waist, length, color):
+        self.waist = waist
+        self.length = length
+        self.color = color
+        self.wearing = False
