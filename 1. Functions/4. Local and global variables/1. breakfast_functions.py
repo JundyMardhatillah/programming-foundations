05117cfc3ee0d7ab10d23e8ac7b1cee4cbd2f82e
Jundy Mardhatillah <jundeveloper92@gmail.com>
2023-02-25 09:44:34 +0700

    describe a pancake with however many ingredients

--- 1. Functions/4. Local and global variables/1. breakfast_functions.py
+++ 1. Functions/4. Local and global variables/1. breakfast_functions.py
@@ -15,7 +15,7 @@ def make_omelette(ingredient):
 
 def make_pancake():
     mix_and_cook()
-    pancake = 'a delicious pancake'
+    pancake = 'a {} pancake'
     return pancake
 
 def fancy_omelette(*ingredients):
