cccf8872efabe6b46953a7620a4dbb49a580a71a
Jundy Mardhatillah <jundeveloper92@gmail.com>
2023-02-26 12:04:54 +0700

    create a closet full of clothes

--- 2. Objects/4. Mutable and immutable data/commands_outfits_and_words.py
+++ 2. Objects/4. Mutable and immutable data/commands_outfits_and_words.py
@@ -1,1 +1,6 @@
 """ ===| You Can Change an Outfit, But You Can't Change Your Words |=== """
+
+# create a closet full of clothes
+closet = ['shirt','hat','pants','jacket','socks']
+print(closet)
+print(id(closet))
